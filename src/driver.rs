27ad00c19314958e2c320fb5debfac5e31c4ad5e
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2024-06-01 08:27:57 -0700

    Simplify cli args handling.

--- src/driver.rs
+++ src/driver.rs
@@ -33,60 +33,6 @@ struct CliArgs {
     until_asm_emission: bool,
 }
 
-#[derive(Debug)]
-struct AppParams {
-    src_filepath: SrcFilepath,
-    compiler_driver_until: CompilerDriverUntil,
-}
-impl TryFrom<CliArgs> for AppParams {
-    type Error = anyhow::Error;
-    fn try_from(args: CliArgs) -> Result<Self> {
-        Ok(Self {
-            src_filepath: SrcFilepath::try_from(&args.src_filepath[..])?,
-            compiler_driver_until: CompilerDriverUntil::from(&args),
-        })
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-enum CompilerUntil {
-    Lexer,
-    Parser,
-    Tacky,
-    AsmCodegen,
-    AsmEmission,
-}
-#[derive(Clone, Copy, Debug)]
-enum CompilerDriverUntil {
-    Compiler(CompilerUntil),
-    Linker,
-}
-impl<'a> From<&'a CliArgs> for CompilerDriverUntil {
-    fn from(args: &'a CliArgs) -> Self {
-        if args.until_asm_emission {
-            Self::Compiler(CompilerUntil::AsmEmission)
-        } else if args.until_asm_codegen {
-            Self::Compiler(CompilerUntil::AsmCodegen)
-        } else if args.until_tacky {
-            Self::Compiler(CompilerUntil::Tacky)
-        } else if args.until_parser {
-            Self::Compiler(CompilerUntil::Parser)
-        } else if args.until_lexer {
-            Self::Compiler(CompilerUntil::Lexer)
-        } else {
-            Self::Linker
-        }
-    }
-}
-impl CompilerDriverUntil {
-    fn compiler_until(&self) -> CompilerUntil {
-        match self {
-            Self::Compiler(u) => u.clone(),
-            Self::Linker => CompilerUntil::AsmEmission,
-        }
-    }
-}
-
 fn preprocess(src_filepath: &SrcFilepath) -> Result<PreprocessedFilepath> {
     let pp_filepath = PreprocessedFilepath::from(src_filepath);
 
@@ -113,9 +59,9 @@ fn preprocess(src_filepath: &SrcFilepath) -> Result<PreprocessedFilepath> {
     Ok(pp_filepath)
 }
 
-fn compile(pp_filepath: PreprocessedFilepath, until: CompilerUntil) -> Result<Option<AsmFilepath>> {
+fn compile(pp_filepath: PreprocessedFilepath, args: &CliArgs) -> Result<Option<AsmFilepath>> {
     let lexer = Lexer::try_from(&pp_filepath)?;
-    if until == CompilerUntil::Lexer {
+    if args.until_lexer {
         let tokens = lexer.collect::<Result<Vec<_>>>()?;
         println!("tokens: {tokens:?}");
         return Ok(None);
@@ -123,19 +69,19 @@ fn compile(pp_filepath: PreprocessedFilepath, until: CompilerUntil) -> Result<Op
 
     let mut parser = Parser::new(lexer);
     let c_prog = parser.parse_program()?;
-    if until == CompilerUntil::Parser {
+    if args.until_parser {
         println!("c_prog: {c_prog:?}");
         return Ok(None);
     }
 
     let tacky_prog = Tackifier::tackify_program(c_prog);
-    if until == CompilerUntil::Tacky {
+    if args.until_tacky {
         println!("tacky_prog: {tacky_prog:?}");
         return Ok(None);
     }
 
     let asm_prog = AsmCodeGenerator::gen_program(tacky_prog);
-    if until == CompilerUntil::AsmCodegen {
+    if args.until_asm_codegen {
         println!("asm_prog: {asm_prog:?}");
         return Ok(None);
     }
@@ -143,7 +89,11 @@ fn compile(pp_filepath: PreprocessedFilepath, until: CompilerUntil) -> Result<Op
     let asm_filepath = AsmFilepath::from(&pp_filepath);
     let asm_emitter = AsmCodeEmitter::try_from(&asm_filepath)?;
     asm_emitter.emit_program(asm_prog)?;
-    return Ok(Some(asm_filepath));
+    if args.until_asm_emission {
+        return Ok(None);
+    }
+
+    Ok(Some(asm_filepath))
 }
 
 fn assemble_and_link(asm_filepath: AsmFilepath) -> Result<ProgramFilepath> {
@@ -176,18 +126,17 @@ pub fn driver_main() -> Result<()> {
     env_logger::init();
 
     let args = CliArgs::parse();
+    log::info!("{args:?}");
 
-    let params = AppParams::try_from(args)?;
-    log::info!("{params:?}");
-
-    let pp_filepath = preprocess(&params.src_filepath)?;
+    let src_filepath = SrcFilepath::try_from(&args.src_filepath[..])?;
+    let pp_filepath = preprocess(&src_filepath)?;
     log::info!("Preprocessor done -> {pp_filepath:?}");
 
-    let asm_filepath = compile(pp_filepath, params.compiler_driver_until.compiler_until())?;
+    let asm_filepath = compile(pp_filepath, &args)?;
     log::info!("Assembly generator done -> {asm_filepath:?}");
 
-    match (params.compiler_driver_until, asm_filepath) {
-        (CompilerDriverUntil::Linker, Some(asm_filepath)) => {
+    match asm_filepath {
+        Some(asm_filepath) => {
             let prog_filepath = assemble_and_link(asm_filepath)?;
             log::info!("Linker done -> {prog_filepath:?}");
         }
