3031c2b15daad28a10e28c7a2c278b13e110c681
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2024-09-14 03:40:38 -0700

    Improve asm_gen::fix api.

--- src/stage4_asm_gen/phase3_fix.rs
+++ src/stage4_asm_gen/phase3_fix.rs
@@ -3,7 +3,6 @@
 use crate::{
     common::types_backend::AssemblyType,
     stage4_asm_gen::{asm_ast::*, phase2_finalize::FinalizedAsmAst},
-    utils::noop,
 };
 
 pub struct OperandFixer {}
@@ -14,36 +13,31 @@ impl OperandFixer {
         in_instrs.flat_map(|in_instr| match in_instr {
             Instruction::Mov { asm_type, mut src, dst } => {
                 if let (AssemblyType::Longword, Operand::ImmediateValue(i)) = (asm_type, &src) {
-                    src = Operand::ImmediateValue(*i as u32 as u64); // Zero-out most significant bytes.
+                    /* In case this `mov` is truncating from quadword to longword,
+                    zero-out the most significant bytes, in order to prevent warning from gcc's assembler. */
+                    src = Operand::ImmediateValue(*i as u32 as u64);
                 }
 
                 let src_to_reg1 =
                     (src.is_on_mem() && dst.is_on_mem())
-                    || (matches!(asm_type, AssemblyType::Quadword)
-                        && matches!(&src, Operand::ImmediateValue(i) if i32::try_from(*i).is_err())
+                    || (Self::is_imm_outside_i32(asm_type, &src)
                         && dst.is_on_mem());
+                let src_to_reg1 = src_to_reg1.then_some(asm_type);
 
                 let new_instr = |src: Operand, dst: Operand| Instruction::Mov { asm_type, src, dst };
 
-                Self::maybe_use_2_regs(
-                    (asm_type, src),
-                    (asm_type, dst),
-                    src_to_reg1,
-                    (false, false),
-                    new_instr,
-                )
+                Self::maybe_use_2_regs(src, dst, src_to_reg1, None, new_instr)
             }
             Instruction::Movsx { src, dst } => {
                 let src_to_reg1 = matches!(&src, Operand::ImmediateValue(_));
+                let src_to_reg1 = src_to_reg1.then_some(AssemblyType::Longword);
+
                 let reg2_to_dst = dst.is_on_mem();
+                let reg2_to_dst = reg2_to_dst.then_some((ToFromReg::FromReg, AssemblyType::Quadword));
+
                 let new_instr = |src: Operand, dst: Operand| Instruction::Movsx { src, dst };
-                Self::maybe_use_2_regs(
-                    (AssemblyType::Longword, src),
-                    (AssemblyType::Quadword, dst),
-                    src_to_reg1,
-                    (false, reg2_to_dst),
-                    new_instr,
-                )
+
+                Self::maybe_use_2_regs(src, dst, src_to_reg1, reg2_to_dst, new_instr)
             }
             Instruction::MovZeroExtend { src, dst } => {
                 if dst.is_on_mem() {
@@ -64,38 +58,34 @@ impl OperandFixer {
             }
             Instruction::Cvttsd2si { dst_asm_type, src, dst } => {
                 let reg2_to_dst = dst.is_on_mem();
+                let reg2_to_dst = reg2_to_dst.then_some((ToFromReg::FromReg, dst_asm_type));
+
                 let new_instr = |src: Operand, dst: Operand| Instruction::Cvttsd2si { dst_asm_type, src, dst };
-                Self::maybe_use_2_regs(
-                    (AssemblyType::Double, src),
-                    (dst_asm_type, dst),
-                    false,
-                    (false, reg2_to_dst),
-                    new_instr
-                )
+
+                Self::maybe_use_2_regs(src, dst, None, reg2_to_dst, new_instr)
             }
             Instruction::Cvtsi2sd { src_asm_type, src, dst } => {
                 let src_to_reg1 = matches!(&src, Operand::ImmediateValue(_));
+                let src_to_reg1 = src_to_reg1.then_some(src_asm_type);
+
                 let reg2_to_dst = matches!(&dst, Operand::Register(_)) == false;
-                let new_instr =|src: Operand, dst: Operand| Instruction::Cvtsi2sd { src_asm_type, src, dst };
-                Self::maybe_use_2_regs(
-                    (src_asm_type, src),
-                    (AssemblyType::Double, dst),
-                    src_to_reg1,
-                    (false, reg2_to_dst),
-                    new_instr
-                )
+                let reg2_to_dst = reg2_to_dst.then_some((ToFromReg::FromReg, AssemblyType::Double));
+
+                let new_instr = |src: Operand, dst: Operand| Instruction::Cvtsi2sd { src_asm_type, src, dst };
+
+                Self::maybe_use_2_regs(src, dst, src_to_reg1, reg2_to_dst, new_instr)
             }
             Instruction::Binary { op, asm_type, arg, tgt } => {
                 use BinaryOperator as BO;
 
                 let (src_to_reg1, dst_to_and_from_reg2);
                 match asm_type {
-                    AssemblyType::Longword|AssemblyType::Quadword => {
-                        src_to_reg1 = (matches!(&op, BO::Add | BO::Sub | BO::And | BO::Or)
+                    AssemblyType::Longword | AssemblyType::Quadword => {
+                        src_to_reg1 =
+                            (matches!(&op, BO::Add | BO::Sub | BO::And | BO::Or)
                                 && arg.is_on_mem()
                                 && tgt.is_on_mem())
-                            || (matches!(&asm_type, AssemblyType::Quadword)
-                                && matches!(&arg, Operand::ImmediateValue(i) if i32::try_from(*i).is_err()));
+                            || Self::is_imm_outside_i32(asm_type, &arg);
                         dst_to_and_from_reg2 = matches!(&op, BinaryOperator::Mul) && tgt.is_on_mem();
                     }
                     AssemblyType::Double => {
@@ -106,22 +96,20 @@ impl OperandFixer {
                         dst_to_and_from_reg2 = matches!(&tgt, Operand::Register(_)) == false;
                     }
                 }
+                let src_to_reg1 = src_to_reg1.then_some(asm_type);
+                let dst_to_and_from_reg2 = dst_to_and_from_reg2.then_some((ToFromReg::ToAndFromReg, asm_type));
+
                 let new_instr = |arg: Operand, tgt: Operand| Instruction::Binary { op, asm_type, arg, tgt };
-                Self::maybe_use_2_regs(
-                    (asm_type, arg),
-                    (asm_type,tgt),
-                    src_to_reg1,
-                    (dst_to_and_from_reg2, dst_to_and_from_reg2),
-                    new_instr,
-                )
+
+                Self::maybe_use_2_regs(arg, tgt, src_to_reg1, dst_to_and_from_reg2, new_instr)
             }
             Instruction::Cmp { asm_type, arg, tgt } => {
                 let (src_to_reg1, dst_to_reg2);
                 match asm_type {
-                    AssemblyType::Longword|AssemblyType::Quadword => {
+                    AssemblyType::Longword | AssemblyType::Quadword => {
                         src_to_reg1 =
                             (arg.is_on_mem() && tgt.is_on_mem())
-                            || (matches!(&arg, Operand::ImmediateValue(i) if i32::try_from(*i).is_err()));
+                            || Self::is_imm_outside_i32(asm_type, &arg);
                         dst_to_reg2 = matches!(&tgt, Operand::ImmediateValue(_));
                     }
                     AssemblyType::Double => {
@@ -129,14 +117,12 @@ impl OperandFixer {
                         dst_to_reg2 = matches!(&tgt, Operand::Register(_)) == false;
                     }
                 }
+                let src_to_reg1 = src_to_reg1.then_some(asm_type);
+                let dst_to_reg2 = dst_to_reg2.then_some((ToFromReg::ToReg, asm_type));
+
                 let new_instr = |arg: Operand, tgt: Operand| Instruction::Cmp { asm_type, arg, tgt };
-                Self::maybe_use_2_regs(
-                    (asm_type, arg),
-                    (asm_type,tgt),
-                    src_to_reg1,
-                    (dst_to_reg2, false),
-                    new_instr,
-                )
+
+                Self::maybe_use_2_regs(arg, tgt, src_to_reg1, dst_to_reg2, new_instr)
             }
             Instruction::Idiv(asm_type, imm @ Operand::ImmediateValue(_)) => {
                 let reg = Register::R10;
@@ -148,7 +134,7 @@ impl OperandFixer {
                 let instr_at_reg = Instruction::Div(asm_type, reg.into());
                 Self::to_reg(asm_type, imm, reg, instr_at_reg)
             }
-            Instruction::Push(operand @ Operand::ImmediateValue(i)) if i32::try_from(i).is_err() => {
+            Instruction::Push(operand) if Self::is_imm_outside_i32(AssemblyType::Quadword, &operand) => {
                 let reg = Register::R10;
                 let instr_at_reg = Instruction::Push(reg.into());
                 Self::to_reg(AssemblyType::Quadword, operand, reg, instr_at_reg)
@@ -156,6 +142,22 @@ impl OperandFixer {
             _ => vec![in_instr],
         })
     }
+
+    /// Some quadword (ie 64-bit) instructions take 32-bit immediate-value operands and sign-extend them to 64 bits.
+    /// This checks whether such sign-extension would corrupt the value.
+    fn is_imm_outside_i32(asm_type: AssemblyType, operand: &Operand) -> bool {
+        (asm_type == AssemblyType::Quadword)
+            && match operand {
+                Operand::ImmediateValue(i) => {
+                    /* We might choose to change `ImmediateValue`'s wrapped rust type, which represents bits.
+                    Here, we explicitly cast the bits to i64, and then evaluate its compatibility with i32. */
+                    let i: i64 = *i as i64;
+                    i32::try_from(i).is_err()
+                }
+                _ => false,
+            }
+    }
+
     fn to_reg(
         asm_type: AssemblyType,
         operand_to_reg: Operand,
@@ -170,22 +172,20 @@ impl OperandFixer {
         vec![instr_to_reg, instr_at_reg]
     }
     fn maybe_use_2_regs(
-        (src_asm_type, mut src): (AssemblyType, Operand),
-        (dst_asm_type, mut dst): (AssemblyType, Operand),
-        src_to_reg1: bool,
-        (dst_to_reg2, reg2_to_dst): (bool, bool),
+        mut src: Operand,
+        mut dst: Operand,
+        src_to_reg1: Option<AssemblyType>,
+        dst_tofrom_reg2: Option<(ToFromReg, AssemblyType)>,
         new_instr: impl FnOnce(Operand, Operand) -> Instruction<FinalizedAsmAst>,
     ) -> Vec<Instruction<FinalizedAsmAst>> {
-        let reg1 = match src_asm_type {
+        let reg1 = |src_asm_type: AssemblyType| match src_asm_type {
             AssemblyType::Longword | AssemblyType::Quadword => Register::R10,
             AssemblyType::Double => Register::XMM14,
         };
-        let reg2 = match dst_asm_type {
+        let reg2 = |dst_asm_type: AssemblyType| match dst_asm_type {
             AssemblyType::Longword | AssemblyType::Quadword => Register::R11,
             AssemblyType::Double => Register::XMM15,
         };
-        let reg1 = move || reg1.into();
-        let reg2 = move || reg2.into();
 
         let mov = |asm_type: AssemblyType, src: Operand, dst: Operand| Instruction::Mov {
             asm_type,
@@ -194,28 +194,29 @@ impl OperandFixer {
         };
 
         let mut instr_to_reg1 = None;
-        if src_to_reg1 {
-            instr_to_reg1 = Some(mov(src_asm_type, src, reg1()));
-            src = reg1();
+        if let Some(src_asm_type) = src_to_reg1 {
+            let reg1 = reg1(src_asm_type);
+            instr_to_reg1 = Some(mov(src_asm_type, src, reg1.into()));
+            src = reg1.into();
         }
 
         let mut instr_to_reg2 = None;
         let mut instr_from_reg2 = None;
-        match (dst_to_reg2, reg2_to_dst) {
-            (false, false) => noop!(),
-            (false, true) => {
-                instr_from_reg2 = Some(mov(dst_asm_type, reg2(), dst));
-                dst = reg2();
+        if let Some((to_from_reg, dst_asm_type)) = dst_tofrom_reg2 {
+            let reg2 = reg2(dst_asm_type);
+            match to_from_reg {
+                ToFromReg::ToReg => {
+                    instr_to_reg2 = Some(mov(dst_asm_type, dst, reg2.into()));
+                }
+                ToFromReg::FromReg => {
+                    instr_from_reg2 = Some(mov(dst_asm_type, reg2.into(), dst));
+                }
+                ToFromReg::ToAndFromReg => {
+                    instr_to_reg2 = Some(mov(dst_asm_type, dst.clone(), reg2.into()));
+                    instr_from_reg2 = Some(mov(dst_asm_type, reg2.into(), dst));
+                }
             }
-            (true, false) => {
-                instr_to_reg2 = Some(mov(dst_asm_type, dst, reg2()));
-                dst = reg2();
-            }
-            (true, true) => {
-                instr_to_reg2 = Some(mov(dst_asm_type, dst.clone(), reg2()));
-                instr_from_reg2 = Some(mov(dst_asm_type, reg2(), dst));
-                dst = reg2();
-            }
+            dst = reg2.into();
         }
 
         let instr = new_instr(src, dst);
@@ -225,4 +226,11 @@ impl OperandFixer {
             .flatten()
             .collect::<Vec<_>>()
     }
+}
+
+#[allow(clippy::enum_variant_names)]
+enum ToFromReg {
+    ToReg,
+    FromReg,
+    ToAndFromReg,
 }
