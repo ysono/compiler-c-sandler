75f6fe5b3497893d927aa686bb0054306019f3af
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2024-10-07 18:14:36 -0700

    [ch14] Singleton .hash() and .eq() don't recurse.

--- src/ds_n_a/singleton.rs
+++ src/ds_n_a/singleton.rs
@@ -1,42 +1,142 @@
-use derive_more::Deref;
-use std::borrow::Borrow;
+#[cfg(test)]
+mod test;
+
+use core::borrow::Borrow;
+use core::hash::{Hash, Hasher};
+use core::ops::Deref;
 use std::collections::HashSet;
-use std::hash::Hash;
 use std::rc::Rc;
 
-#[derive(Deref, PartialEq, Eq, Hash, Debug)]
-pub struct Singleton<T>(Rc<T>);
-impl<T> Clone for Singleton<T> {
-    fn clone(&self) -> Self {
-        Self(Rc::clone(&self.0))
-    }
-}
-impl<T> Borrow<T> for Singleton<T> {
-    fn borrow(&self) -> &T {
-        &self.0
-    }
-}
-
+/// This container stores distinct instances of `T`.
+///
+/// "Singleton" refers to each of these instances of `T`; there may be multiple instances of `T`.
 pub struct SingletonRepository<T> {
-    repo: HashSet<Singleton<T>>,
+    repo: HashSet<SingletonKey<T>>,
 }
 impl<T> Default for SingletonRepository<T> {
+    /// [`SingletonRepository`] is `Default` regardless of whether `T` is `Default`.
     fn default() -> Self {
-        Self { repo: Default::default() }
+        Self { repo: HashSet::default() }
     }
 }
-impl<T> SingletonRepository<T>
-where
-    T: Hash + Eq,
-{
+impl<T: Hash + Eq> SingletonRepository<T> {
     pub fn get_or_new(&mut self, elem: T) -> Singleton<T> {
-        match self.repo.get(&elem) {
+        /*
+        1) Calculate `elem.hash()`: `T::hash()`. The digest determines the bucket.
+        2) In the bucket, for each existing_key, evaluate `existing_key.borrow() == &elem`: `SingletonKey::borrow() == &T`.
+        In the special case where `T` recursively contains a `Singleton<U>` member,
+            `T::hash()` and `T::eq()` both ought to calculate non-recursively.
+        */
+        let existing_key = self.repo.get(&elem);
+
+        match existing_key {
             None => {
-                let elem = Singleton(Rc::new(elem));
-                self.repo.insert(elem.clone());
-                elem
+                let singleton = Singleton { rc: Rc::new(elem) };
+                let singleton_ret = singleton.clone();
+                let new_key = SingletonKey { singleton };
+
+                /*
+                1) Calculate `new_key.hash()`: `SingletonKey::hash()`, which in turn calculates `T::hash()`. The digest determines the bucket.
+                2) In the bucket, for each existing_key, evaluate `&existing_key == &new_key`: `&SingletonKey == &SingletonKey`.
+                    Each existing key is expected to be non-equal to the new key.
+                */
+                let did_insert = self.repo.insert(new_key);
+                debug_assert!(did_insert);
+
+                singleton_ret
             }
-            Some(singleton) => singleton.clone(),
+            Some(SingletonKey { singleton: node }) => node.clone(),
         }
     }
 }
+
+/// The key type of a hash table that's queryable by `T`.
+///
+/// In order to enable querying a hash table by `T`,
+/// + [`SingletonKey`] is `impl Borrow<T>`.
+/// + [`SingletonKey::hash()`] and `T::hash()` derive the same digest.
+///
+/// This type is constructable within this mod only.
+/// [`SingletonRepository`] constructs exactly one instance of [`SingletonKey`] per distinct `T`.
+/// In addition, [`SingletonKey`] is non-`Clone`.
+/// Hence, all instances of [`SingletonKey`] are non-equal to one another.
+///
+/// It is possibly also correct for [`SingletonKey::eq()`] to return `false` statically.
+/// [`SingletonKey::eq()`] must compare by pointer value if any of:
+/// + An instance is compared against itself during some (unknown by us) internal mechanism of `HashMap`.
+/// + If [`SingletonKey`] were `Clone`. -- We might make it `Clone` if we choose to make this type public.
+///
+/// Caveat: [`SingletonKey::eq()`] works as expected solely on two instances that were constructed by the same [`SingletonRepository`].
+#[derive(Debug)]
+struct SingletonKey<T> {
+    singleton: Singleton<T>,
+}
+impl<T> Borrow<T> for SingletonKey<T> {
+    fn borrow(&self) -> &T {
+        self.singleton.rc.as_ref()
+    }
+}
+impl<T: Hash> Hash for SingletonKey<T> {
+    /// (This `impl` is the same as `#[derive(Hash)]`; we choose to be explicit.)
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let t: &T = self.singleton.rc.as_ref();
+        t.hash(state);
+    }
+}
+impl<T> PartialEq<Self> for SingletonKey<T> {
+    fn eq(&self, other: &Self) -> bool {
+        Rc::ptr_eq(&self.singleton.rc, &other.singleton.rc)
+    }
+}
+impl<T> Eq for SingletonKey<T> {}
+
+/// A shared pointer to a uniquely-constructed distinct instance of `T`.
+///
+/// This type is constructable within this mod only.
+/// [`SingletonRepository`] constructs exactly one instance of [`Singleton`] per distinct `T`.
+/// In addition, [`Singleton`] is `Clone`.
+/// Hence, the equality between 2 instances of [`Singleton`] is determined exactly by the equality of the pointer values.
+///
+/// Both [`Singleton::hash()`] and [`Singleton::eq()`] evaluate the pointer value, and not the content of `T`, so that
+///     in case `T` recursively contains [`Singleton<U>`] member(s),
+///     the default `T::hash()` and `T::eq()`, as provided by `#[derive(Hash, PartialEq)] struct T{...}`,
+///     calculate non-recursively, without evaluating nested `U`(s).
+///
+/// [`Singleton::hash()`] differs from `T::hash()`.
+/// In order to prevent [`Singleton`] from being used as the key type of a hash table that's queryable by `T`,
+/// [`Singleton`] is _not_ `impl Borrow<T>`.
+///
+/// Caveat: [`Singleton::eq()`] works as expected solely on two instances that were constructed by the same [`SingletonRepository`].
+#[derive(Debug)]
+pub struct Singleton<T> {
+    rc: Rc<T>,
+}
+impl<T> Clone for Singleton<T> {
+    /// [`Singleton`] is `Clone` regardless of whether `T` is `Clone`.
+    fn clone(&self) -> Self {
+        Self { rc: Rc::clone(&self.rc) }
+    }
+}
+impl<T> AsRef<T> for Singleton<T> {
+    fn as_ref(&self) -> &T {
+        self.rc.as_ref()
+    }
+}
+impl<T> Deref for Singleton<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        self.rc.as_ref()
+    }
+}
+impl<T: Hash> Hash for Singleton<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let ptr = Rc::as_ptr(&self.rc);
+        ptr.hash(state);
+    }
+}
+impl<T> PartialEq<Self> for Singleton<T> {
+    fn eq(&self, other: &Self) -> bool {
+        Rc::ptr_eq(&self.rc, &other.rc)
+    }
+}
+impl<T> Eq for Singleton<T> {}
---
+++ src/ds_n_a/singleton/test.rs
@@ -0,0 +1,297 @@
+use super::*;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+/// Using the default `impl` for `Hash` and `PartialEq`.
+#[derive(Hash, PartialEq, Eq, Debug)]
+enum LLNode {
+    Terminal(Datum),
+    Recursive(Datum, Singleton<Self>),
+}
+
+/// Using the default `impl` for `Hash` and `PartialEq`.
+#[derive(Hash, PartialEq, Eq, Debug)]
+struct Datum {
+    content: i32,
+    stats: Rc<Stats>,
+}
+impl Datum {
+    fn new(content: i32) -> (Self, Rc<Stats>) {
+        let stats = Rc::new(Stats::default());
+        let stats_ret = Rc::clone(&stats);
+        let slf = Self { content, stats };
+        (slf, stats_ret)
+    }
+}
+
+#[derive(Default, Debug)]
+struct Stats {
+    hash_ct: AtomicUsize,
+    eq_ct: AtomicUsize,
+}
+impl Hash for Stats {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.hash_ct.fetch_add(1, Ordering::SeqCst);
+
+        /* Don't update the Hasher. */
+        let _ = state;
+    }
+}
+impl PartialEq<Self> for Stats {
+    fn eq(&self, other: &Self) -> bool {
+        self.eq_ct.fetch_add(1, Ordering::SeqCst);
+        other.eq_ct.fetch_add(1, Ordering::SeqCst);
+
+        /* No interference with the equality calculation. */
+        true
+    }
+}
+impl Eq for Stats {}
+impl Stats {
+    fn hash_ct_since_last(&self) -> usize {
+        self.hash_ct.swap(0, Ordering::SeqCst)
+    }
+    fn eq_ct_since_last(&self) -> usize {
+        self.eq_ct.swap(0, Ordering::SeqCst)
+    }
+}
+
+#[derive(Debug)]
+struct ExpectedMethodCalls {
+    hash: bool,
+    eq: bool,
+}
+fn assert_method_calls(stats: &Stats, exp: ExpectedMethodCalls) {
+    let hash_ct = stats.hash_ct_since_last();
+    let eq_ct = stats.eq_ct_since_last();
+    let ok = ((hash_ct > 0) == exp.hash) && ((eq_ct > 0) == exp.eq);
+    assert!(ok, "{:?}", (hash_ct, eq_ct, exp));
+}
+
+#[test]
+fn simple_elem() {
+    const CONTENT: i32 = 123;
+    let new_datum = || Datum::new(CONTENT);
+
+    let mut repo = SingletonRepository::<Datum>::default();
+
+    /*
+    ; SingletonKey
+    ;   |
+    ; Singleton
+    ;   |
+    ; Datum         Datum
+    ;   |             |
+    ; Stats         Stats
+    */
+
+    {
+        /* Construct an instance of `Datum`. */
+        let (datum, input_stats) = new_datum();
+        assert_method_calls(&input_stats, ExpectedMethodCalls { hash: false, eq: false });
+
+        /* Expect new singleton. */
+        assert_eq!(repo.repo.len(), 0);
+        let singleton = repo.get_or_new(datum);
+        assert_eq!(repo.repo.len(), 1);
+        assert_eq!(Rc::ptr_eq(&singleton.stats, &input_stats), true);
+        assert_method_calls(
+            &input_stats,
+            ExpectedMethodCalls {
+                hash: true, // 1) `Datum::hash()` determines the bucket.
+                eq: false,  // 2) No existing `SingletonKey::borrow()` to compare against.
+            },
+        );
+    }
+
+    {
+        /* Construct a non-distinct instance of `Datum`. */
+        let (datum, input_stats) = new_datum();
+        assert_method_calls(&input_stats, ExpectedMethodCalls { hash: false, eq: false });
+
+        /* Expect _no_ new singleton. */
+        assert_eq!(repo.repo.len(), 1);
+        let singleton = repo.get_or_new(datum);
+        assert_eq!(repo.repo.len(), 1);
+        assert_eq!(Rc::ptr_eq(&singleton.stats, &input_stats), false);
+        assert_method_calls(
+            &input_stats,
+            ExpectedMethodCalls {
+                hash: true, // 1) `Datum::hash()` determines the bucket.
+                eq: true,   // 2) `Datum::eq()` between `SingletonKey::borrow()` and `&Datum`.
+            },
+        );
+        assert_method_calls(
+            &singleton.stats,
+            ExpectedMethodCalls {
+                hash: false, // Didn't calc `SingletonKey::hash()` on the existing `SingletonKey`.
+                eq: true,    // 2) `Datum::eq()` between `SingletonKey::borrow()` and `&Datum`.
+            },
+        );
+    }
+}
+
+#[test]
+fn recursive_elem() {
+    const CONTENT: i32 = 123;
+    let new_terminal_node = || {
+        let (datum, datum_stats) = Datum::new(CONTENT);
+        let term_node = LLNode::Terminal(datum);
+        (term_node, datum_stats)
+    };
+    let new_recursive_node = |nested_node: Singleton<LLNode>| {
+        let (datum, datum_stats) = Datum::new(CONTENT);
+        let rec_node = LLNode::Recursive(datum, nested_node);
+        (rec_node, datum_stats)
+    };
+
+    let mut repo = SingletonRepository::<LLNode>::default();
+
+    /*
+    ; SingletonKey
+    ;   |
+    ; Singleton
+    ;   |
+    ; LLNode::Terminal      LLNode::Terminal
+    ;   |                     |
+    ; Datum                 Datum
+    ;   |                     |
+    ; Stats                 Stats
+    */
+
+    {
+        /* Construct a `LLNode::Terminal`. */
+        let (term_node, input_stats) = new_terminal_node();
+        assert_method_calls(&input_stats, ExpectedMethodCalls { hash: false, eq: false });
+
+        /* Expect new singleton. */
+        assert_eq!(repo.repo.len(), 0);
+        let singleton = repo.get_or_new(term_node);
+        assert_eq!(repo.repo.len(), 1);
+        match singleton.as_ref() {
+            LLNode::Terminal(stored) => {
+                assert_eq!(Rc::ptr_eq(&stored.stats, &input_stats), true);
+                assert_method_calls(&input_stats, ExpectedMethodCalls { hash: true, eq: false });
+                /* Same situation as the SingletonRepository<Datum> test case. */
+            }
+            LLNode::Recursive(..) => unreachable!(),
+        }
+    }
+
+    let (stored_term, stored_term_stats);
+    {
+        /* Construct a non-distinct instance of `LLNode::Terminal`. */
+        let (term_node, input_stats) = new_terminal_node();
+        assert_method_calls(&input_stats, ExpectedMethodCalls { hash: false, eq: false });
+
+        /* Expect _no_ new singleton. */
+        assert_eq!(repo.repo.len(), 1);
+        let singleton = repo.get_or_new(term_node);
+        assert_eq!(repo.repo.len(), 1);
+        match singleton.as_ref() {
+            LLNode::Terminal(stored) => {
+                assert_eq!(Rc::ptr_eq(&stored.stats, &input_stats), false);
+                assert_method_calls(&input_stats, ExpectedMethodCalls { hash: true, eq: true });
+                assert_method_calls(&stored.stats, ExpectedMethodCalls { hash: false, eq: true });
+                /* Same situation as the SingletonRepository<Datum> test case. */
+
+                stored_term_stats = stored.stats.clone();
+            }
+            LLNode::Recursive(..) => unreachable!(),
+        }
+
+        stored_term = singleton;
+    };
+
+    /*
+    ; SingletonKey
+    ;   |
+    ; Singleton
+    ;   |
+    ; LLNode::Recursive   LLNode::Recursive
+    ;   |         \         /         |
+    ; Datum        Singleton        Datum
+    ;   |             |               |
+    ; Stats     LLNode::Terminal    Stats
+    ;                 |
+    ;               Datum
+    ;                 |
+    ;               Stats
+    */
+
+    {
+        /* Construct a `LLNode::Recursive`. */
+        let (rec_node, input_stats) = new_recursive_node(stored_term.clone());
+        assert_method_calls(&input_stats, ExpectedMethodCalls { hash: false, eq: false });
+        assert_method_calls(
+            &stored_term_stats,
+            ExpectedMethodCalls { hash: false, eq: false },
+        );
+
+        /* Expect new singleton. */
+        assert_eq!(repo.repo.len(), 1);
+        let singleton = repo.get_or_new(rec_node);
+        assert_eq!(repo.repo.len(), 2);
+        match singleton.as_ref() {
+            LLNode::Terminal(..) => unreachable!(),
+            LLNode::Recursive(stored_rec, _nested_node) => {
+                assert_eq!(Rc::ptr_eq(&stored_rec.stats, &input_stats), true);
+                assert_method_calls(
+                    &input_stats,
+                    ExpectedMethodCalls {
+                        hash: true, // 1) `LLNode::hash()` determines the bucket.
+                        eq: false, // 2) No existing `SingletonKey::borrow()` containing `LLNode::Recursive` to compare against.
+                    },
+                ); // Same situation as the SingletonRepository<Datum> test case.
+                assert_method_calls(
+                    &stored_term_stats,
+                    ExpectedMethodCalls {
+                        hash: false, // 1) Calc'ing `LLNode::hash()` entails calc'ing `Singleton::hash()`, which doesn't recurse into calc'ing `Datum::hash()`.
+                        eq: false, // 2) No existing `SingletonKey::borrow()` containing `LLNode::Recursive` to compare against.
+                    },
+                ); // Newly pertinent in the SingletonRepository<LLNode> test case.
+            }
+        }
+    }
+
+    {
+        /* Construct a non-distinct instance of `LLNode::Recursive`. */
+        let (rec_node, input_stats) = new_recursive_node(stored_term.clone());
+        assert_method_calls(&input_stats, ExpectedMethodCalls { hash: false, eq: false });
+        assert_method_calls(
+            &stored_term_stats,
+            ExpectedMethodCalls { hash: false, eq: false },
+        );
+
+        /* Expect _no_ new singleton. */
+        assert_eq!(repo.repo.len(), 2);
+        let singleton = repo.get_or_new(rec_node);
+        assert_eq!(repo.repo.len(), 2);
+        match singleton.as_ref() {
+            LLNode::Terminal(..) => unreachable!(),
+            LLNode::Recursive(stored_rec, _nested_node) => {
+                assert_eq!(Rc::ptr_eq(&stored_rec.stats, &input_stats), false);
+                assert_method_calls(
+                    &input_stats,
+                    ExpectedMethodCalls {
+                        hash: true, // 1) `LLNode::hash()` determines the bucket.
+                        eq: true, // 2) `LLNode::eq()` between `SingletonKey::borrow()` and `&LLNode`.
+                    },
+                ); // Same situation as the SingletonRepository<Datum> test case.
+                assert_method_calls(
+                    &stored_rec.stats,
+                    ExpectedMethodCalls {
+                        hash: false, // Didn't calc `SingletonKey::hash()` on the existing `SingletonKey`.
+                        eq: true, // 2) `LLNode::eq()` between `SingletonKey::borrow()` and `&LLNode`.
+                    },
+                ); // Same situation as the SingletonRepository<Datum> test case.
+                assert_method_calls(
+                    &stored_term_stats,
+                    ExpectedMethodCalls {
+                        hash: false, // 1) Calc'ing `LLNode::hash()` entails calc'ing `Singleton::hash()`, which doesn't recurse into calc'ing `Datum::hash()`.
+                        eq: false,   // 2) Calc'ing `LLNode::eq()` ... ditto ... doesn't recurse.
+                    },
+                ); // Newly pertinent in the SingletonRepository<LLNode> test case.
+            }
+        }
+    }
+}
