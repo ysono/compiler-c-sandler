a7bb051b3ad3fa573bd2dadfdce0e879699a71d4
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2024-10-07 21:51:18 -0700

    Refactor tacky gen_exp_*() into their own mod. (Cut&paste w/ trivial adaptations.)

--- src/stage3_tacky/generate.rs
+++ src/stage3_tacky/generate.rs
@@ -5,6 +5,7 @@
 
 mod ary;
 mod conditional;
+mod exp;
 mod looping;
 mod misc;
 mod obj;
@@ -161,60 +162,6 @@ impl<'a> FunInstrsGenerator<'a> {
             }
             c::Statement::For(loop_id, foor) => self.gen_stmt_for(loop_id, foor),
             c::Statement::Null => noop!(),
-        }
-    }
-
-    /* Expression */
-
-    fn gen_exp_and_get_value(
-        &mut self,
-        exp: c::TypedExpression<c::Expression<TypeCheckedCAst>>,
-    ) -> Value {
-        match self.gen_exp(exp) {
-            ExpResult::Value(val) => val,
-            ExpResult::Object(obj) => self.convert_object_to_value(obj),
-        }
-    }
-    fn gen_exp(
-        &mut self,
-        c::TypedExpression { exp, typ }: c::TypedExpression<c::Expression<TypeCheckedCAst>>,
-    ) -> ExpResult {
-        match exp {
-            c::Expression::Const(konst) => Value::Constant(konst).into(),
-            c::Expression::Var(ident) => Object::Direct(ident).into(),
-            c::Expression::Cast(c_cast) => self.gen_exp_cast(c_cast).into(),
-            c::Expression::Unary(c_unary) => self.gen_exp_unary(c_unary, typ).into(),
-            c::Expression::Binary(c_binary) => self.gen_exp_binary(c_binary, typ).into(),
-            c::Expression::Assignment(c_assign) => self.gen_exp_assignment(c_assign).into(),
-            c::Expression::Conditional(c_cond) => self.gen_exp_conditional(c_cond, typ).into(),
-            c::Expression::FunctionCall(c_fun_call) => {
-                self.gen_exp_fun_call(c_fun_call, typ).into()
-            }
-            c::Expression::Dereference(c_deref) => self.gen_exp_deref(c_deref, typ).into(),
-            c::Expression::AddrOf(c_addrof) => self.gen_exp_addrof(c_addrof, typ).into(),
-        }
-    }
-    fn gen_exp_lvalue(
-        &mut self,
-        c::TypedExpression { exp, typ }: c::TypedExpression<c::LvalueExpression<TypeCheckedCAst>>,
-    ) -> Object {
-        match exp {
-            c::LvalueExpression::Var(ident) => Object::Direct(ident),
-            c::LvalueExpression::Dereference(c_deref) => self.gen_exp_deref(c_deref, typ),
-        }
-    }
-    /// Aka lvalue-converting an lvalue expression.
-    fn convert_object_to_value(&mut self, object: Object) -> Value {
-        match object {
-            Object::Direct(ident) => Value::Variable(ident),
-            Object::Pointee { addr, typ } => {
-                let dst = self.symbol_table.declare_var_anon(typ);
-                self.instrs.push(Instruction::Load(Load {
-                    src_addr: addr,
-                    dst: Rc::clone(&dst),
-                }));
-                Value::Variable(dst)
-            }
         }
     }
 }
