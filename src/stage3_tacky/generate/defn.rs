45fccc630aa2596d102abd45f761b036022dca91
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2024-10-25 00:50:07 -0700

    [ch16] Tacky initializes runtime arr-of-characters <- string initializer.

--- src/stage3_tacky/generate/defn.rs
+++ src/stage3_tacky/generate/defn.rs
@@ -7,7 +7,7 @@ use crate::{
     stage2_parse::{c_ast as c, phase3_typecheck::TypeCheckedCAst},
     stage3_tacky::tacky_ast::*,
 };
-use std::rc::Rc;
+use std::{cmp, rc::Rc};
 
 impl<'a> FunInstrsGenerator<'a> {
     pub(super) fn gen_var_defn(
@@ -20,8 +20,12 @@ impl<'a> FunInstrsGenerator<'a> {
                 InitializerItem::Single(typed_exp) => {
                     self.gen_var_init_single(&ident, &mut cur_offset, typed_exp);
                 }
-                InitializerItem::String { .. } => todo!(),
-                InitializerItem::Pointer(_) => todo!(),
+                InitializerItem::String { chars, zeros_sfx_bytelen } => {
+                    self.gen_var_init_string(&ident, &mut cur_offset, chars, zeros_sfx_bytelen);
+                }
+                InitializerItem::Pointer(_) => unreachable!(
+                    "The pointer initializer item is used to initialize static objs only."
+                ),
                 InitializerItem::Zero(zeros_bytelen) => {
                     self.gen_var_init_zero(&ident, &mut cur_offset, zeros_bytelen);
                 }
@@ -42,6 +46,58 @@ impl<'a> FunInstrsGenerator<'a> {
         self.instrs.push(new_cto(val, ident, *cur_offset));
 
         *cur_offset += single_bytelen;
+    }
+
+    fn gen_var_init_string(
+        &mut self,
+        ident: &Rc<SymbolIdentifier>,
+        cur_offset: &mut ByteLen,
+        chars: Vec<u8>,
+        zeros_sfx_bytelen: ByteLen,
+    ) {
+        let mut rem_bytelen = chars.len() as u64 + zeros_sfx_bytelen.as_int();
+
+        let mut cur_chars_sfx: &[u8] = &chars;
+
+        fn derive_window_bytes<const LEN: usize>(chars_sfx: &[u8]) -> ([u8; LEN], usize) {
+            let mut buf = [0u8; LEN];
+
+            let copy_bytelen = cmp::min(chars_sfx.len(), LEN);
+            buf[0..copy_bytelen].copy_from_slice(&chars_sfx[0..copy_bytelen]);
+
+            (buf, copy_bytelen)
+        }
+
+        while rem_bytelen >= 8 {
+            let (buf, copy_bytelen) = derive_window_bytes::<8>(cur_chars_sfx);
+
+            let val = Value::Constant(Const::ULong(u64::from_le_bytes(buf)));
+            self.instrs.push(new_cto(val, ident, *cur_offset));
+
+            cur_chars_sfx = &cur_chars_sfx[copy_bytelen..];
+            *cur_offset += ByteLen::new(8);
+            rem_bytelen -= 8;
+        }
+        if rem_bytelen >= 4 {
+            let (buf, copy_bytelen) = derive_window_bytes::<4>(cur_chars_sfx);
+
+            let val = Value::Constant(Const::UInt(u32::from_le_bytes(buf)));
+            self.instrs.push(new_cto(val, ident, *cur_offset));
+
+            cur_chars_sfx = &cur_chars_sfx[copy_bytelen..];
+            *cur_offset += ByteLen::new(4);
+            rem_bytelen -= 4;
+        }
+        while rem_bytelen >= 1 {
+            let (buf, copy_bytelen) = derive_window_bytes::<1>(cur_chars_sfx);
+
+            let val = Value::Constant(Const::UChar(u8::from_le_bytes(buf)));
+            self.instrs.push(new_cto(val, ident, *cur_offset));
+
+            cur_chars_sfx = &cur_chars_sfx[copy_bytelen..];
+            *cur_offset += ByteLen::new(1);
+            rem_bytelen -= 1;
+        }
     }
 
     fn gen_var_init_zero(
