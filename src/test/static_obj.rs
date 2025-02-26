4155698edfa868445ee0ee46d9035c7d25655882
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2025-02-25 16:27:27 -0800

    [lint] integ test: static_obj: Clean up.

--- src/test/static_obj.rs
+++ src/test/static_obj.rs
@@ -11,6 +11,7 @@ use crate::{
     test::utils,
 };
 use anyhow::Result;
+use std::{collections::VecDeque, rc::Rc};
 
 fn is_ident_exact(ident: &SymbolIdentifier, expected_exact: &str) -> bool {
     match ident {
@@ -51,35 +52,25 @@ fn static_double_objs() -> Result<()> {
     assert!(static_rw_objs.is_empty());
 
     {
-        let mut static_ro_objs = static_ro_objs.into_iter();
-        {
-            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.next().unwrap();
+        fn expect_ro_double(
+            static_ro_objs: &mut VecDeque<(Rc<SymbolIdentifier>, ROAttrs)>,
+            expected_alignment: Alignment,
+            expected_double: f64,
+        ) {
+            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.pop_front().unwrap();
             assert!(is_ident_generated(&ident));
-            assert_eq!(alignment, Alignment::B8);
+            assert_eq!(alignment, expected_alignment);
             assert_eq!(
                 initializer,
-                StaticInitializerItem::Single(Const::Double(3.14))
+                StaticInitializerItem::Single(Const::Double(expected_double))
             );
         }
-        {
-            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.next().unwrap();
-            assert!(is_ident_generated(&ident));
-            assert_eq!(alignment, Alignment::B16);
-            assert_eq!(
-                initializer,
-                StaticInitializerItem::Single(Const::Double(-0.0))
-            );
-        }
-        {
-            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.next().unwrap();
-            assert!(is_ident_generated(&ident));
-            assert_eq!(alignment, Alignment::B8);
-            assert_eq!(
-                initializer,
-                StaticInitializerItem::Single(Const::Double((1u64 << 63) as f64))
-            );
-        }
-        assert!(static_ro_objs.next().is_none());
+
+        let mut static_ro_objs = VecDeque::from(static_ro_objs);
+        expect_ro_double(&mut static_ro_objs, Alignment::B8, 3.14);
+        expect_ro_double(&mut static_ro_objs, Alignment::B16, -0.0);
+        expect_ro_double(&mut static_ro_objs, Alignment::B8, (1u64 << 63) as f64);
+        assert!(static_ro_objs.is_empty());
     }
 
     Ok(())
@@ -87,142 +78,117 @@ fn static_double_objs() -> Result<()> {
 
 #[test]
 fn static_string_objs() -> Result<()> {
-    let pp = "
-        char static_arr_ch_05[5]  = \"AABBB\";
-        char static_arr_ch_21[21] = \"CCDDD\";
+    let pp = r#"
+        char static_arr_char_05[5]  = "AABBB";
+        char static_arr_char_21[21] = "CCDDD";
 
-        char *static_ptr_ch_05 = \"aabbb\";
-        char *static_ptr_ch_17 = \"ccccddddeeeefffff\";
+        char *static_ptr_char_05 = "aabbb";
+        char *static_ptr_char_17 = "ccccddddeeeefffff";
 
         int main(void) {
-            char rt_arr_ch_05[5]  = \"EEFFF\";
-            char rt_arr_ch_21[21] = \"GGHHH\";
+            char rt_arr_char_05[5]  = "EEFFF";
+            char rt_arr_char_21[21] = "GGHHH";
 
-            char *rt_ptr_ch_05 = \"eefff\";
-            char *rt_ptr_ch_17 = \"gggghhhhiiiijjjjj\";
+            char *rt_ptr_char_05 = "eefff";
+            char *rt_ptr_char_17 = "gggghhhhiiiijjjjj";
 
-            \"kklll\"[2];
+            "kklll"
+            "mmnnn"[2];
+
+            "AABBB"[2];
+            "aabbb"[2];
+            "EEFFF"[2];
+            "eefff"[2];
         }
-        ";
+        "#;
 
     let (_t_prog, be_symtab) = utils::compile_until_asm_gen(&pp)?;
 
     let (static_rw_objs, static_ro_objs) = utils::be_symtab_into_static_objs(be_symtab);
 
     {
-        let mut static_rw_objs = static_rw_objs.into_iter();
+        fn expect_rw(
+            static_rw_objs: &mut VecDeque<(Rc<SymbolIdentifier>, RWAttrs)>,
+            expected_ident: &'static str,
+            expected_alignment: Alignment,
+        ) -> Vec<StaticInitializerItem> {
+            let (ident, RWAttrs { alignment, initializer, .. }) =
+                static_rw_objs.pop_front().unwrap();
+            assert!(is_ident_exact(&ident, expected_ident));
+            assert_eq!(alignment, expected_alignment);
+            initializer.unwrap()
+        }
+
+        let mut static_rw_objs = VecDeque::from(static_rw_objs);
         {
-            let (ident, RWAttrs { alignment, initializer, .. }) = static_rw_objs.next().unwrap();
-            assert!(is_ident_exact(&ident, "static_arr_ch_05"));
-            assert_eq!(alignment, Alignment::B1);
+            let inits = expect_rw(&mut static_rw_objs, "static_arr_char_05", Alignment::B1);
             assert_eq!(
-                initializer,
-                Some(vec![StaticInitializerItem::String(InitializerString {
+                inits,
+                vec![StaticInitializerItem::String(InitializerString {
                     chars: "AABBB".into(),
                     zeros_sfx_bytelen: ByteLen::new(0)
-                })])
+                })]
             );
         }
         {
-            let (ident, RWAttrs { alignment, initializer, .. }) = static_rw_objs.next().unwrap();
-            assert!(is_ident_exact(&ident, "static_arr_ch_21"));
-            assert_eq!(alignment, Alignment::B16);
+            let inits = expect_rw(&mut static_rw_objs, "static_arr_char_21", Alignment::B16);
             assert_eq!(
-                initializer,
-                Some(vec![StaticInitializerItem::String(InitializerString {
+                inits,
+                vec![StaticInitializerItem::String(InitializerString {
                     chars: "CCDDD".into(),
                     zeros_sfx_bytelen: ByteLen::new(21 - 5)
-                })])
+                })]
             );
         }
         {
-            let (ident, RWAttrs { alignment, initializer, .. }) = static_rw_objs.next().unwrap();
-            assert!(is_ident_exact(&ident, "static_ptr_ch_05"));
-            assert_eq!(alignment, Alignment::B8);
+            let inits = expect_rw(&mut static_rw_objs, "static_ptr_char_05", Alignment::B8);
             assert!(matches!(
-                &initializer.unwrap()[..],
+                &inits[..],
                 [
                     StaticInitializerItem::Pointer(rhs_ident)
                 ] if is_ident_generated(rhs_ident)
             ));
         }
         {
-            let (ident, RWAttrs { alignment, initializer, .. }) = static_rw_objs.next().unwrap();
-            assert!(is_ident_exact(&ident, "static_ptr_ch_17"));
-            assert_eq!(alignment, Alignment::B8);
+            let inits = expect_rw(&mut static_rw_objs, "static_ptr_char_17", Alignment::B8);
             assert!(matches!(
-                &initializer.unwrap()[..],
+                &inits[..],
                 [
                     StaticInitializerItem::Pointer(rhs_ident)
                 ] if is_ident_generated(rhs_ident)
             ));
         }
-        assert!(static_rw_objs.next().is_none());
+        assert!(static_rw_objs.is_empty());
     }
 
     {
-        let mut static_ro_objs = static_ro_objs.into_iter();
-        {
-            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.next().unwrap();
+        fn expect_ro(
+            static_ro_objs: &mut VecDeque<(Rc<SymbolIdentifier>, ROAttrs)>,
+            expected_chars: &'static str,
+        ) {
+            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.pop_front().unwrap();
             assert!(is_ident_generated(&ident));
             assert_eq!(alignment, Alignment::B1);
             assert_eq!(
                 initializer,
                 StaticInitializerItem::String(InitializerString {
-                    chars: "aabbb".into(),
+                    chars: expected_chars.into(),
                     zeros_sfx_bytelen: ByteLen::new(1)
-                }),
+                })
             );
         }
-        {
-            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.next().unwrap();
-            assert!(is_ident_generated(&ident));
-            assert_eq!(alignment, Alignment::B1);
-            assert_eq!(
-                initializer,
-                StaticInitializerItem::String(InitializerString {
-                    chars: "ccccddddeeeefffff".into(),
-                    zeros_sfx_bytelen: ByteLen::new(1)
-                }),
-            );
-        }
-        {
-            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.next().unwrap();
-            assert!(is_ident_generated(&ident));
-            assert_eq!(alignment, Alignment::B1);
-            assert_eq!(
-                initializer,
-                StaticInitializerItem::String(InitializerString {
-                    chars: "eefff".into(),
-                    zeros_sfx_bytelen: ByteLen::new(1)
-                }),
-            );
-        }
-        {
-            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.next().unwrap();
-            assert!(is_ident_generated(&ident));
-            assert_eq!(alignment, Alignment::B1);
-            assert_eq!(
-                initializer,
-                StaticInitializerItem::String(InitializerString {
-                    chars: "gggghhhhiiiijjjjj".into(),
-                    zeros_sfx_bytelen: ByteLen::new(1)
-                }),
-            );
-        }
-        {
-            let (ident, ROAttrs { alignment, initializer }) = static_ro_objs.next().unwrap();
-            assert!(is_ident_generated(&ident));
-            assert_eq!(alignment, Alignment::B1);
-            assert_eq!(
-                initializer,
-                StaticInitializerItem::String(InitializerString {
-                    chars: "kklll".into(),
-                    zeros_sfx_bytelen: ByteLen::new(1)
-                }),
-            );
-        }
-        assert!(static_ro_objs.next().is_none());
+
+        let mut static_ro_objs = VecDeque::from(static_ro_objs);
+        expect_ro(&mut static_ro_objs, "aabbb");
+        expect_ro(&mut static_ro_objs, "ccccddddeeeefffff");
+        expect_ro(&mut static_ro_objs, "eefff");
+        expect_ro(&mut static_ro_objs, "gggghhhhiiiijjjjj");
+        expect_ro(&mut static_ro_objs, "kklllmmnnn");
+        expect_ro(&mut static_ro_objs, "AABBB");
+        expect_ro(&mut static_ro_objs, "aabbb");
+        expect_ro(&mut static_ro_objs, "EEFFF");
+        expect_ro(&mut static_ro_objs, "eefff");
+        assert!(static_ro_objs.is_empty());
     }
 
     Ok(())
